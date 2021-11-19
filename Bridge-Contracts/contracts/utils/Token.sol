5e621b44f659df4067a338a71efd093ce4957326
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-20 00:22:06 +0530

    add: added Toke contract base

---
+++ Bridge-Contracts/contracts/utils/Token.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    address public admin;
+
+    constructor(string memory name, string memory symbol)
+        public
+        ERC20(name, symbol)
+    {
+        admin = msg.sender;
+    }
+
+    // method for updating the admin
+
+    function updateAdmin(address newAdmin) external onlyAdmin {
+        admin = newAdmin;
+    }
+
+    // method for minting "amount" tokens for "to" address
+    function mint(address to, uint256 amount) external onlyAdmin {
+        _mint(to, amount);
+    }
+
+    function burn(address owner, uint256 amount) external onlyAdmin {
+        _burn(owner, amount);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only Admin can access this method");
+        _;
+    }
+}
