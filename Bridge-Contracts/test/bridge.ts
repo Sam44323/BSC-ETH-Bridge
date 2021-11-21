52aa3909b2af746732099019c01b025c3bc52731
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 20:48:06 +0530

    add: added testing file for Bridge

---
+++ Bridge-Contracts/test/bridge.ts
@@ -0,0 +1,9 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Bridge-Contract testing", function () {
+  it("Testing", async () => {
+    expect(true).to.be.true;
+  });
+});
