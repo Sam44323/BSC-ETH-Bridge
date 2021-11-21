7c00626c617b4a58a1dc1d59fb870cac51b79812
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 15:12:14 +0530

    add: added base file for bridge

---
+++ Bridge-Contracts/contracts/base/Bridge.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Bridge {
+    address public admin;
+    IERC20 public token;
+    uint256 public nonce;
+    mapping(uint256 => bool) public processedTransactionNonces;
+}
