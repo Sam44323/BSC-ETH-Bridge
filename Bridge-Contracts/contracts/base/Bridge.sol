772c571283f5fcbafc987caa9fd67cd17c93f6bb
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 16:40:12 +0530

    add: added custom token based IToken

--- Bridge-Contracts/contracts/base/Bridge.sol
+++ Bridge-Contracts/contracts/base/Bridge.sol
@@ -2,18 +2,47 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../base/IToken.sol";
 
 // the base implementation contract for the bridge
 
 contract Bridge {
     address public admin;
-    IERC20 public token;
+    IToken public immutable token;
     uint256 public nonce;
     mapping(uint256 => bool) public processedTransactionNonces;
+
+    enum Step {
+        Burn,
+        Mint
+    }
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        uint256 date,
+        uint256 nonce,
+        Step indexed step
+    );
 
     // initializing the bridge with the token contract and the admin address
     constructor(address _token) {
         admin = msg.sender;
-        token = IERC20(_token); // getting the token from the IERC interface
+        token = IToken(_token); // getting the token from the IERC interface
+    }
+
+    // burn some amount of tokens
+    function burn(address to, uint256 _amount) public {
+        token.burn(msg.sender, _amount);
+        emit Transfer(
+            msg.sender,
+            to,
+            _amount,
+            block.timestamp,
+            nonce,
+            Step.Burn
+        );
+        nonce++;
     }
 }
---
+++ Bridge-Contracts/contracts/base/IToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IToken {
+    function mint(address to, uint256 amount) external;
+
+    function burn(address owner, uint256 amount) external;
+}
