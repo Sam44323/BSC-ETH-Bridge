c3cd7429d1e6cbd88383f107dbf7ec8a005df716
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 14:38:40 +0530

    add: added contract for TokenETH

---
+++ Bridge-Contracts/contracts/Tokens/TokenETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../base/Token.sol";
+
+contract TokenETH is Token {
+    // creating a token for BSC
+    constructor() Token("ETH Token", "ETK") {}
+}
