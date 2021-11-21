129d122cfff0f56cc73f0c96c6523b5c4b49a93d
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 19:58:23 +0530

    remove: removed the ignore for test

---
+++ Bridge-Contracts/test/index.ts
@@ -0,0 +1,22 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Token } from "../typechain/Token";
+
+describe("Token", function () {
+  let Token,
+    token: Token,
+    owner: SignerWithAddress,
+    addr1: SignerWithAddress,
+    addr2: SignerWithAddress;
+
+  beforeEach(async () => {
+    Token = await ethers.getContractFactory("TokenBSC");
+    token = await Token.deploy();
+    [owner, addr1, addr2] = await ethers.getSigners();
+  });
+
+  it("Admin should be the deployer", async function () {
+    expect(await token.admin()).to.equal(owner.address);
+  });
+});
