0b64e165c23767c859f3f7f52e9c22b6997dd7fc
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 17:04:33 +0530

    fix: fixed the contract name for BSC

--- Bridge-Contracts/contracts/bridge/BridgeBSC.sol
+++ Bridge-Contracts/contracts/bridge/BridgeBSC.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 import "../base/Bridge.sol";
 
-contract BridgeETH is Bridge {
+contract BridgeBSC is Bridge {
     constructor(address token_address) Bridge(token_address) {}
 }
