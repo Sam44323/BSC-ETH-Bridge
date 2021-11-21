870a64ab9f169fc6f7e311177741d0b295c43b2f
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 20:58:31 +0530

    add: added BSC and ETH bridge contract deployer along with the tokens

--- Bridge-Contracts/test/bridge.ts
+++ Bridge-Contracts/test/bridge.ts
@@ -1,9 +1,34 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { BridgeETH, BridgeBSC, Bridge, TokenETH, TokenBSC } from "../typechain";
 
 describe("Bridge-Contract testing", function () {
-  it("Testing", async () => {
-    expect(true).to.be.true;
+  let BridgeRoot: Bridge,
+    bscBridge: BridgeBSC,
+    ethBridge: BridgeETH,
+    ethToken: TokenETH,
+    bscToken: TokenBSC,
+    owner,
+    addr1,
+    addr2;
+  beforeEach(async () => {
+    // deploying tokens for the bscBridge and the ethBridge
+    bscToken = await (await ethers.getContractFactory("TokenBSC")).deploy();
+    bscToken = await (await ethers.getContractFactory("TokenETH")).deploy();
+
+    // deploying the contracts for the ethBridge and the bscBridge
+    bscBridge = await (
+      await ethers.getContractFactory("BridgeBSC")
+    ).deploy(bscToken.address);
+    ethBridge = await (
+      await ethers.getContractFactory("BridgeETH")
+    ).deploy(ethToken.address);
+    [owner, addr1, addr2] = await ethers.getSigners();
+  });
+
+  it("Test", async () => {
+    console.log(BridgeRoot);
+    expect(true).to.not.be.undefined;
   });
 });
