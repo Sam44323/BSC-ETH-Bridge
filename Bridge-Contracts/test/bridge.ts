245ac79bed434c31419a4dd9b068d6c90e97c540
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-21 21:35:10 +0530

    add: added test for burning tokens using approvals

--- Bridge-Contracts/test/bridge.ts
+++ Bridge-Contracts/test/bridge.ts
@@ -9,13 +9,13 @@ describe("Bridge-Contract testing", function () {
     ethBridge: BridgeETH,
     ethToken: TokenETH,
     bscToken: TokenBSC,
-    owner,
-    addr1,
-    addr2;
+    owner: SignerWithAddress,
+    addr1: SignerWithAddress,
+    addr2: SignerWithAddress;
   beforeEach(async () => {
     // deploying tokens for the bscBridge and the ethBridge
     bscToken = await (await ethers.getContractFactory("TokenBSC")).deploy();
-    bscToken = await (await ethers.getContractFactory("TokenETH")).deploy();
+    ethToken = await (await ethers.getContractFactory("TokenETH")).deploy();
 
     // deploying the contracts for the ethBridge and the bscBridge
     bscBridge = await (
@@ -24,11 +24,27 @@ describe("Bridge-Contract testing", function () {
     ethBridge = await (
       await ethers.getContractFactory("BridgeETH")
     ).deploy(ethToken.address);
+
     [owner, addr1, addr2] = await ethers.getSigners();
+
+    // minting some tokens for the eth token and the bsc token
+    await ethToken.mint(owner.address, 1000);
+    await bscToken.mint(owner.address, 1000);
+
+    // transferring some tokens for the eth token and the bsc token to addr1
+    await ethToken.connect(owner).transfer(addr1.address, 100);
+    await bscToken.connect(owner).transfer(addr1.address, 100);
   });
 
-  it("Test", async () => {
-    console.log(BridgeRoot);
-    expect(true).to.not.be.undefined;
+  it("Test the burning for ETH bridge contract", async () => {
+    // burning some tokens for the eth token
+
+    // approving the bridge contract to have a transaction using the tokens
+    await ethToken.connect(addr1).approve(ethBridge.address, 100);
+    await ethBridge.connect(addr1).burn(10);
+    const balance = await (await ethToken.balanceOf(addr1.address)).toString();
+    // checking the balance of the eth token for addr1
+    expect(balance).to.be.equal("90");
+    // expect(await ethToken.balanceOf(addr1.address)).to.be.equal(90);
   });
 });
