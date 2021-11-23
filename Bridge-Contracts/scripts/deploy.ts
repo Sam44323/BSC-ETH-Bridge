f05f8afad168610cd9f3b6f8d78d9b686dd0bd57
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-23 21:24:51 +0530

    add: added deploy script along with verifications

--- Bridge-Contracts/scripts/deploy.ts
+++ Bridge-Contracts/scripts/deploy.ts
@@ -1,12 +1,29 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
+
+/*
+ If you have contracts with similar bytecodes, you can use the scripts in this format for verification
+ npx hardhat verify --network <network-name> <contract-address> --contract "contracts/Tokens/TokenETH.sol:TokenETH(contract file path)" 
+ */
 
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ETHToken = await ethers.getContractFactory("TokenETH");
+  const ethToken = await ETHToken.deploy();
 
-  await greeter.deployed();
+  console.log("Deploying...");
+  await ethToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  try {
+    console.log("Verifying TokenETH...");
+    await run("verify:verify", {
+      address: ethToken.address,
+      contract: "contracts/Tokens/TokenETH.sol:TokenETH",
+      constructorArguments: [],
+    });
+  } catch (e: any) {
+    console.error(e.message);
+  }
+
+  console.log("ETH Token deployed to:", ethToken.address);
 }
 
 main()
