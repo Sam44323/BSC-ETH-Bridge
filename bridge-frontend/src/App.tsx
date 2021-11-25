2caf4ce3daac966d9c735a2a6d59556442c8fd05
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 00:34:29 +0530

    add: added styles for header for the bridge

--- bridge-frontend/src/App.tsx
+++ bridge-frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./App.module.scss";
 import BridgeContainer from "./container/BridgeContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsAltH } from "@fortawesome/free-solid-svg-icons";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <section className={styles.HeaderContainer}>
-        <h1>Bridge</h1>
+        <h1>
+          Bridge
+          <br /> <span>ETH</span>{" "}
+          <FontAwesomeIcon
+            icon={faArrowsAltH}
+            style={{
+              color: "salmon",
+            }}
+          />{" "}
+          <span>BSC</span>
+        </h1>
       </section>
       <BridgeContainer />
     </div>
