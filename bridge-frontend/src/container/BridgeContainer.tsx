0eb6a85f3dc50fe1c716e9dcfaa4e261c90bc003
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 00:15:29 +0530

    add: added package for fontawesome

--- bridge-frontend/src/container/BridgeContainer.tsx
+++ bridge-frontend/src/container/BridgeContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/container/BridgeContainer.module.scss";
 
 const BridgeContainer: React.FC = () => {
-  return <div></div>;
+  return <div className={styles.BridgeContainer}></div>;
 };
 
 export default BridgeContainer;
