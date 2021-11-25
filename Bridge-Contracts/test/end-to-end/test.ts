ee8708a662d85b7baa11950c2a5d6d18c225762f
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-25 18:28:50 +0530

    add: added inital feature creator for the descriptor in e2e test

--- Bridge-Contracts/test/end-to-end/test.ts
+++ Bridge-Contracts/test/end-to-end/test.ts
@@ -19,6 +19,32 @@ describe("Bridging from ETH to BSC and vice-versa", () => {
 
   beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
+    bscToken = await (await ethers.getContractFactory("TokenBSC"))
+      .connect(owner)
+      .deploy();
+    ethToken = await (await ethers.getContractFactory("TokenETH"))
+      .connect(owner)
+      .deploy();
+
+    bscBridge = await (await ethers.getContractFactory("BridgeBSC"))
+      .connect(owner)
+      .deploy(bscToken.address);
+    ethBridge = await (await ethers.getContractFactory("BridgeETH"))
+      .connect(owner)
+      .deploy(ethToken.address);
+
+    await ethToken.transfer(ethBridge.address, 1000);
+    await bscToken.transfer(bscBridge.address, 1000);
+
+    // transferring some tokens for the eth token and the bsc token to addr1
+    await ethToken.transfer(addr1.address, 100);
+    await bscToken.transfer(addr2.address, 100);
+
+    console.log((await ethToken.balanceOf(ethBridge.address)).toString());
+    console.log((await bscToken.balanceOf(bscBridge.address)).toString());
+
+    console.log((await ethToken.balanceOf(addr1.address)).toString());
+    console.log((await bscToken.balanceOf(addr2.address)).toString());
   });
 
   it("Bridge from ETH to BSC", async () => {});
