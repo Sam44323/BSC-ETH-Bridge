366ffd705b4a324e78b62e6d2bedfd6e9e061ccf
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 00:15:53 +0530

    update: updated the containerization

--- bridge-frontend/src/container/BridgeContainer.tsx
+++ bridge-frontend/src/container/BridgeContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/container/BridgeContainer.module.scss";
 
 const BridgeContainer: React.FC = () => {
-  return <div className={styles.BridgeContainer}></div>;
+  return <div></div>;
 };
 
 export default BridgeContainer;
