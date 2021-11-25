c222519d3b228a4ffc6910b0614828c89ce1840b
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 00:12:47 +0530

    add: added container stylings for the styles

---
+++ bridge-frontend/src/container/BridgeContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/container/BridgeContainer.module.scss";
+
+const BridgeContainer: React.FC = () => {
+  return <div></div>;
+};
+
+export default BridgeContainer;
