cd6f7b2a044edd96a7d0715e698b9a483bcd6bb3
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 14:13:00 +0530

    update: updated the stylings for the background

--- bridge-frontend/src/App.tsx
+++ bridge-frontend/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
           <FontAwesomeIcon
             icon={faArrowsAltH}
             style={{
-              color: "salmon",
+              color: "#fff",
             }}
           />{" "}
           <span>BSC</span>
