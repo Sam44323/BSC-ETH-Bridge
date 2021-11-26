496ce0256de65350d977e78aa6f58d928dde7b41
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 14:19:17 +0530

    update: updated the swap to dropdown containe

--- bridge-frontend/src/components/Dropdown.tsx
+++ bridge-frontend/src/components/Dropdown.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/components/SwapContainer.module.scss";
+import styles from "../styles/components/Dropdown.module.scss";
 
 const SwapContainer: React.FC = () => {
   return <div></div>;
