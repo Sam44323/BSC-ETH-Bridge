f2b11b6e05dd940d274d55b4d8ac685e9e4acac1
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-26 22:50:40 +0530

    add: added docs for input

--- bridge-frontend/src/components/Input.tsx
+++ bridge-frontend/src/components/Input.tsx
@@ -9,6 +9,17 @@ interface InputProps {
   placeholder: string;
 }
 
+/**
+ *
+ * @param props value: string to be displayed in the input
+ * @param props type: type of input
+ * @param props changeValue: function to be called when the input is changed
+ * @param props label: label of the input
+ * @param props placeholder: placeholder of the input
+ *
+ * @returns a input component configured with the given props
+ */
+
 const Input: React.FC<InputProps> = (props) => {
   return (
     <div className={styles.InputContainer}>
