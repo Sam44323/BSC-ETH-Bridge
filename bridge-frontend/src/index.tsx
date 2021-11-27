f6b87941b73c6ff41718fbf717ad9983c687a07b
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-27 16:25:42 +0530

    add: added an header component

--- bridge-frontend/src/index.tsx
+++ bridge-frontend/src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ChainId, Config, DAppProvider } from "@usedapp/core";
+
+const config: Config = {
+  supportedChains: [ChainId.Rinkeby, 97],
+  notifications: {
+    expirationPeriod: 1000,
+    checkInterval: 1000,
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
