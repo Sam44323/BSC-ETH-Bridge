cd4c955c5ce4ba8eb90a88c2780c34f7d7e48cc0
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-27 17:24:30 +0530

    add: added balance renderer with connection for wallet

--- bridge-frontend/src/components/Header.tsx
+++ bridge-frontend/src/components/Header.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../styles/components/Header.module.scss";
 import { ethereum, binance } from "../assets/index";
+import { useEthers, useEtherBalance, useTokenBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
 
 import Button from "./Button";
 
 const Header: React.FC = () => {
+  const [accountData, setAccountData] = React.useState<any>(null);
+  const { account, activateBrowserWallet } = useEthers();
+  const balance = useTokenBalance(
+    "0x902c63Dd4ec70ab6e1351c18070147a1bf2D017B",
+    account
+  );
+
+  useEffect(() => setAccountData(account), [account]);
+
+  console.log(balance);
+
   return (
     <div className={styles.HeaderContainer}>
       <section className={styles.ButtonContainer}>
-        <Button clickHandler={() => {}}>Connect Wallet</Button>
+        <Button
+          clickHandler={() => activateBrowserWallet(() => alert("false"))}
+        >
+          {accountData
+            ? `${accountData?.substring(0, 4)}...${accountData?.substring(
+                accountData.length - 4
+              )}`
+            : "Connect Wallet"}
+        </Button>
       </section>
       <section className={styles.BalanceSection}>
         <img src={ethereum} alt="logo" />
-        <p>100 ETK</p>
+        <p>{balance && formatEther(balance).slice(0, 6)} ETK</p>
       </section>
     </div>
   );
