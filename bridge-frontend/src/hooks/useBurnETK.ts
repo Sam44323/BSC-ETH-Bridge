f4bfa3a6f18e93860898786b5b44cdfc872d1564
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-30 00:10:13 +0530

    add: added token fetcher for the etk bridge hook

--- bridge-frontend/src/hooks/useBurnETK.ts
+++ bridge-frontend/src/hooks/useBurnETK.ts
@@ -1,15 +1,29 @@
 import { useEthers } from "@usedapp/core";
-import { abi as ETKToken } from "../abis/Tokens/TokenETH.sol/TokenETH.json";
-import { abi as ETHBridge } from "../abis/bridge/BridgeETH.sol/BridgeETH.json";
 import { toast } from "react-toastify";
+import { getContracts } from "../utils/contracts";
+import { useEffect, useState } from "react";
 
 const useBurnETK = () => {
   // contract for the etk token on ethereum(rinkeby)
+  const [contractsData, setContractsData] = useState<any>();
   const { account } = useEthers();
+
+  useEffect(() => {
+    const getContract = async () => {
+      const data = await getContracts();
+      setContractsData({
+        token: data[1],
+        bridge: data[3],
+      });
+    };
+    getContract();
+  }, []);
 
   // method for approving to burn some amount of the etk token mentioned by user
 
-  const approveETKBurn = async (amount: string) => {};
+  const approveETKBurn = async (amount: string) => {
+    console.log(contractsData);
+  };
 
   const burnETK = async (amount: string) => {};
 
