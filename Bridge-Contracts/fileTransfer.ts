df560ea82b3cb8b0dbfb09dcfcd9aefe49e5c2da
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-02 22:51:52 +0530

    add: added file transfer editor for backend abi copy support

--- Bridge-Contracts/fileTransfer.ts
+++ Bridge-Contracts/fileTransfer.ts
@@ -1,13 +1,21 @@
 import { ncp } from "ncp";
 
-const [source, destination] = [
+const [source, frontDest, backendDest] = [
   "./artifacts/contracts/",
   "../bridge-frontend/src/abis/",
+  "../bridge-backend/abis/",
 ];
 
-ncp(source, destination, function (err) {
+ncp(source, frontDest, function (err) {
   if (err) {
     return console.error(err);
   }
-  console.log("done!");
+  console.log("Copied to frontend abis!");
+});
+
+ncp(source, backendDest, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Copied to backend abis!");
 });
