4a5d3d481a3669184649995ede794c6df90a5f61
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-04 00:34:18 +0530

    add: added abstraction for the fetcher for amount

--- bridge-backend/controllers/minter.controller.ts
+++ bridge-backend/controllers/minter.controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from "express";
+import Web3 from "web3";
 import logger from "../utils/logger";
 import { getWeb3 } from "../utils/web3";
+
+const amountFetcher = async (txHash: string, web3: Web3) => {
+  const receipt = await web3.eth.getTransaction(txHash);
+  let amount = web3.utils.hexToNumberString(`0x${receipt.input.slice(35)}`);
+  amount = web3.utils.fromWei(amount, "ether");
+  return amount;
+};
 
 export const mintETH = async (req: Request, res: Response) => {
   try {
     const { txHash } = req.body;
     console.log(txHash);
     setTimeout(() => logger.info("Waiting for block to be added!"), 3000);
-    const web3 = getWeb3("ETH");
-    const receipt = await web3.eth.getTransactionReceipt(txHash);
-    console.log(receipt);
+    console.log(await amountFetcher(txHash, getWeb3("ETH")));
 
     res.status(200).json({
       message: "working!",
@@ -28,8 +34,8 @@ export const mintBSC = async (req: Request, res: Response) => {
     console.log(txHash);
     setTimeout(() => logger.info("Waiting for block to be added!"), 3000);
     const web3 = getWeb3("BSC");
-    const receipt = await web3.eth.getTransactionReceipt(txHash);
-    console.log(receipt);
+    const receipt = await web3.eth.getTransaction(txHash);
+    console.log(receipt.input);
 
     res.status(200).json({
       message: "working!",
