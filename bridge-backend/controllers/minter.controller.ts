e20adc777e329738fc2e16cce1c78f81f35e4786
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-04 00:40:27 +0530

    fix: fixed the minting web3 instance error for controllers

--- bridge-backend/controllers/minter.controller.ts
+++ bridge-backend/controllers/minter.controller.ts
@@ -3,6 +3,13 @@ import Web3 from "web3";
 import logger from "../utils/logger";
 import { getWeb3 } from "../utils/web3";
 
+/**
+ *
+ * @param txHash the burn hash for the transaction that's being processed by the user
+ * @param web3 the web3 instance that should be used to connect with the chain for getting the data
+ * @returns the amount burned and verification for the transaction
+ */
+
 const amountFetcher = async (txHash: string, web3: Web3) => {
   const receipt = await web3.eth.getTransaction(txHash);
   let amount = web3.utils.hexToNumberString(`0x${receipt.input.slice(35)}`);
@@ -10,12 +17,18 @@ const amountFetcher = async (txHash: string, web3: Web3) => {
   return amount;
 };
 
+/**
+ * method for starting the minting process on ethereum
+ * @param req the request object
+ * @param res the response object
+ */
+
 export const mintETH = async (req: Request, res: Response) => {
   try {
     const { txHash } = req.body;
     console.log(txHash);
     setTimeout(() => logger.info("Waiting for block to be added!"), 3000);
-    const burnedAmount = await amountFetcher(txHash, getWeb3("ETH"));
+    const burnedAmount = await amountFetcher(txHash, getWeb3("BSC"));
     console.log(burnedAmount);
 
     res.status(200).json({
@@ -29,17 +42,22 @@ export const mintETH = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * method for starting the minting process on binance
+ * @param req the request object
+ * @param res the response object
+ */
+
 export const mintBSC = async (req: Request, res: Response) => {
   try {
     const { txHash } = req.body;
     console.log(txHash);
     setTimeout(() => logger.info("Waiting for block to be added!"), 3000);
-    const web3 = getWeb3("BSC");
-    const receipt = await web3.eth.getTransaction(txHash);
-    console.log(receipt.input);
+    const burnedAmount = await amountFetcher(txHash, getWeb3("ETH"));
+    console.log(burnedAmount);
 
     res.status(200).json({
-      message: "working!",
+      message: "Minting in progress!",
     });
   } catch (err: any) {
     logger.error(`Can't fetch the colleges at this moment: ${err.message}`);
