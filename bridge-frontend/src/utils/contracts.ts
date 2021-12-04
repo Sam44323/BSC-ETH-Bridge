8ce59faa806bd8a633a00db38fe054f46d67bb2a
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-05 00:54:51 +0530

    update: updated the frontend contract fetcher structure

--- bridge-frontend/src/utils/contracts.ts
+++ bridge-frontend/src/utils/contracts.ts
@@ -1,11 +1,23 @@
 import { Contracts } from "./config";
 import { getWeb3 } from "./web3";
+import { BridgeBSC } from "../types/BridgeBSC";
+import { BridgeETH } from "../types/BridgeETH";
+import { TokenBSC } from "../types/TokenBSC";
+import { TokenETH } from "../types/TokenETH";
+import Web3 from "web3";
+
+const contractGenerator = (abi: any, address: any, web3: Web3) =>
+  new web3.eth.Contract(abi, address);
 
 export const getContracts = async () => {
   const web3 = getWeb3();
-  const contractInstances = Contracts.map(
-    (contract) => new web3.eth.Contract(contract.abi, contract.address)
-  );
+  const [btk, etk, bscB, ethB] = Contracts;
+  const contractInstances = [
+    contractGenerator(btk.abi, btk.address, web3) as unknown as TokenBSC,
+    contractGenerator(etk.abi, etk.address, web3) as unknown as TokenETH,
+    contractGenerator(bscB.abi, bscB.address, web3) as unknown as BridgeBSC,
+    contractGenerator(ethB.abi, ethB.address, web3) as unknown as BridgeETH,
+  ];
 
   return contractInstances;
 };
